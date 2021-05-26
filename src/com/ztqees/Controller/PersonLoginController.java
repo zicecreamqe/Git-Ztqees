0208f5a4b0198c06e6ff0787fc72072afd2ab718
ztqees <1428450575@qq.com>
2021-05-26 12:42:23 +0800

    加了控制层的一些东西

--- src/com/ztqees/Controller/PersonLoginController.java
+++ src/com/ztqees/Controller/PersonLoginController.java
@@ -1,4 +1,13 @@
 package com.ztqees.Controller;
 
-public class PersonLoginController {
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class PersonLoginController extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().write("hhh");
+    }
 }
